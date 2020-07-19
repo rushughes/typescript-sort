d72abad1c4d9673a8af80b6e6a97b7f1abdb7c1b
Rus Hughes <russell.hughes@gmail.com>
2020-07-19 11:13:11 +0200

    we are now using an abstract class

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,13 @@ import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { Sorter } from './Sorter';
 
-// const numbersCollection = new NumbersCollection([11, 3, -5, 0, 99]);
-// const sorter = new Sorter(numbersCollection);
-// console.log(numbersCollection.data);
-// sorter.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([11, 3, -5, 0, 99]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection('XPotato123');
-// const sorter = new Sorter(charactersCollection);
-// console.log(charactersCollection.data);
-// sorter.sort();
-// console.log(charactersCollection.data);
+const charactersCollection = new CharactersCollection('XPotato123');
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
@@ -21,8 +17,5 @@ linkedList.add(-10);
 linkedList.add(1);
 linkedList.add(5);
 linkedList.add(0);
-
-const sorter = new Sorter(linkedList);
-linkedList.print();
-sorter.sort();
+linkedList.sort();
 linkedList.print();
