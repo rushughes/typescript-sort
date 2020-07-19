b227424eb42f8391fbe9845361d82a29cc9d085b
Rus Hughes <russell.hughes@gmail.com>
2020-07-19 09:54:13 +0200

    new Sorter and NumbersCollection classes to make the sort more generic

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,8 @@
-class Sorter {
-  constructor(public collection: number[]) {}
+import { NumbersCollection } from './NumbersCollection';
+import { Sorter } from './Sorter';
 
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
-console.log(sorter.collection);
+const numbersCollection = new NumbersCollection([11, 3, -5, 0, 99]);
+const sorter = new Sorter(numbersCollection);
+console.log(numbersCollection.data);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
