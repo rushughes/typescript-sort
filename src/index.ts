51d45f73c5ee6907d0474564675b18053bbe4af5
Rus Hughes <russell.hughes@gmail.com>
2020-07-19 08:53:04 +0200

    new sort class

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
-console.log('hi there');
-console.log('boo');
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+console.log(sorter.collection);
+sorter.sort();
+console.log(sorter.collection);
